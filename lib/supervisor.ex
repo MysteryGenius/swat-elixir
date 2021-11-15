93489e5db59ddde070cce8180df688878d0b97f7
MysteryGenius <tanjianzhen3000@gmail.com>
2021-11-15 18:28:44 +0800

    feat: initial commit

---
+++ lib/supervisor.ex
@@ -0,0 +1,10 @@
+defmodule Learn.Supervisor do
+	use Application
+
+	def start(_type, _args) do
+		children = [
+			{Plug.Cowboy, scheme: :http, plug: Learn.Plug, options: [port: 4001]}
+		]
+		Supervisor.start_link(children, strategy: :one_for_one)
+	end
+end
