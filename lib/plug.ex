99ed58661228fd1f624954a2b99c41d6cef426ca
MysteryGenius <tanjianzhen3000@gmail.com>
2021-11-15 20:25:19 +0800

    feat: added load_hits

--- lib/plug.ex
+++ lib/plug.ex
@@ -4,8 +4,23 @@ defmodule Learn.Plug do
 	def init(opts), do: opts
 
 	def call(conn, _opts) do
+		content = load_hits("count.txt")
+
 		conn
 		|> put_resp_content_type("text/plain")
-		|> send_resp(200, "Hello world")
+		|> send_resp(200, content)
+	end
+
+	@doc """
+	load_hits/1 reads the hits from a file and returns the value
+	"""
+	def load_hits(filepath) do
+		case File.read(filepath) do
+			{:ok, body}      ->
+				body
+			{:error, :enoent} ->
+				File.write!(filepath, "1")
+				load_hits(filepath)
+		end
 	end
 end
